fa091bc68b038d5dd4b4e392a43e84841f3e2dec
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2023-11-23 13:22:47 +0100

    Create create_schemas.sql

---
+++ create_schemas.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+CREATE SCHEMA IF NOT EXISTS {fdc_admin}; -- parametre + historik på modeller
+CREATE SCHEMA IF NOT EXISTS {fdc_lookup}; -- lookup data
+CREATE SCHEMA IF NOT EXISTS {fdc_sector}; -- sektor data
+CREATE SCHEMA IF NOT EXISTS {fdc_flood}; -- oversvømmelses data
+CREATE SCHEMA IF NOT EXISTS {fdc_values}; -- parametre + historik/ oversigt over modelkørsler
+CREATE SCHEMA IF NOT EXISTS {fdc_results}; -- resultater
+
+
