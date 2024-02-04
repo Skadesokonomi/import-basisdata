2d0c57db9358c9cd129f1d9ea445e922c3867d48
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2024-02-04 15:58:45 +0100

    Update lookup_tourism.sql

--- lookup_tourism.sql
+++ lookup_tourism.sql
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS turisme (
 
 TRUNCATE turisme;
 
-INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (520, '(UDFASES) Bygning til feriekoloni, vandrehjem o.lign. bortset fra sommerhus', 10, 2362);
-INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (331, 'Hotel, kro eller konferencecenter med overnatning', 50, 2362);
-INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (332, 'Bed & breakfast mv.', 8, 2362);
-INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (510, 'Bygninger til sommerhus', 6, 2362);
-INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (521, 'Feriecenter, center til campingplads mv.', 100, 2362);
-INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (522, 'Bygning med ferielejligheder til erhvervsmæssig udlejning', 50, 2362);
+INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (520, '(UDFASES) Bygning til feriekoloni, vandrehjem o.lign. bortset fra sommerhus', 10, 1100);
+INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (331, 'Hotel, kro eller konferencecenter med overnatning', 50, 2000);
+INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (332, 'Bed & breakfast mv.', 8, 1100);
+INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (510, 'Bygninger til sommerhus', 4, 750);
+INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (521, 'Feriecenter, center til campingplads mv.', 100, 850);
+INSERT INTO turisme (bbr_anv_kode, bbr_anv_tekst, kapacitet, omkostning) VALUES (522, 'Bygning med ferielejligheder til erhvervsmæssig udlejning', 50, 750);
 
 ALTER TABLE ONLY turisme ADD CONSTRAINT turisme_pkey PRIMARY KEY (bbr_anv_kode);
 
