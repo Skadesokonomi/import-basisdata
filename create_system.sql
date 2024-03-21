5c8419fbd376c66069ce6740df525b35f889930a
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2024-03-21 15:44:15 +0100

    f_agriculture rettelse

--- create_system.sql
+++ create_system.sql
@@ -1182,3 +1182,52 @@ CREATE TABLE IF NOT EXISTS patches_done
     run_at timestamp,
     CONSTRAINT patches_done_pkey PRIMARY KEY (patch_name)
 );
+
+SET search_path = fdc_admin, public;
+
+INSERT INTO parametre (name, parent, value, type, minval, maxval, lookupvalues, "default", explanation, sort, checkable) VALUES ('q_agriculture', 'Queries', '
+SELECT
+    b.*,
+    k.afgroedegruppe,
+    k.afgroedekategori,
+    p.*,
+    n.*,
+    COALESCE(areal_oversvoem_nutid_m2 * p.{f_price_t_agr_price} /100.00,0.0)::NUMERIC(12,2) AS {f_damage_present_q_agriculture},
+    f.*,
+    COALESCE(areal_oversvoem_fremtid_m2 * p.{f_price_t_agr_price} /100.00,0.0)::NUMERIC(12,2) AS {f_damage_future_q_agriculture},
+    r.*
+    FROM {t_agriculture} b
+    LEFT JOIN {t_agr_cat} k ON k.{f_pkey_t_agr_cat} = b.{f_afgcode_t_agriculture} 
+    LEFT JOIN {t_agr_price} p ON p.{f_pkey_t_agr_price} = k.{f_pcat_t_agr_cat}, 
+    LATERAL (
+        SELECT
+            COUNT (*) AS cnt_oversvoem_nutid,
+            COALESCE(SUM(st_area(st_intersection(b.{f_geom_t_agriculture},{f_geom_Oversvømmelsesmodel, nutid}))),0)::NUMERIC(12,2) AS areal_oversvoem_nutid_m2,
+            COALESCE(MIN({f_depth_Oversvømmelsesmodel, nutid}) * 100.00,0)::NUMERIC(12,2) AS min_vanddybde_nutid_cm,
+            COALESCE(MAX({f_depth_Oversvømmelsesmodel, nutid}) * 100.00,0)::NUMERIC(12,2) AS max_vanddybde_nutid_cm,
+            COALESCE(AVG({f_depth_Oversvømmelsesmodel, nutid}) * 100.00,0)::NUMERIC(12,2) AS avg_vanddybde_nutid_cm
+        FROM {Oversvømmelsesmodel, nutid} WHERE st_intersects(b.{f_geom_t_agriculture},{f_geom_Oversvømmelsesmodel, nutid}) AND {f_depth_Oversvømmelsesmodel, nutid} >= {Minimum vanddybde (meter)}
+    ) n,
+    LATERAL (
+        SELECT
+            COUNT (*) AS cnt_oversvoem_fremtid,
+            COALESCE(SUM(st_area(st_intersection(b.{f_geom_t_agriculture},{f_geom_Oversvømmelsesmodel, fremtid}))),0)::NUMERIC(12,2) AS areal_oversvoem_fremtid_m2,
+            COALESCE(MIN({f_depth_Oversvømmelsesmodel, fremtid}) * 100.00,0)::NUMERIC(12,2) AS min_vanddybde_fremtid_cm,
+            COALESCE(MAX({f_depth_Oversvømmelsesmodel, fremtid}) * 100.00,0)::NUMERIC(12,2) AS max_vanddybde_fremtid_cm,
+            COALESCE(AVG({f_depth_Oversvømmelsesmodel, fremtid}) * 100.00,0)::NUMERIC(12,2) AS avg_vanddybde_fremtid_cm
+        FROM {Oversvømmelsesmodel, fremtid} WHERE st_intersects(b.{f_geom_t_agriculture},{f_geom_Oversvømmelsesmodel, fremtid}) AND {f_depth_Oversvømmelsesmodel, fremtid} >= {Minimum vanddybde (meter)}
+    ) f,
+    LATERAL (
+        SELECT
+          ''{Medtag i risikoberegninger}'' AS risiko_beregning,
+		  {Returperiode, antal år} AS retur_periode,
+          ((
+		      0.219058829 * CASE WHEN ''{Medtag i risikoberegninger}'' IN (''Skadebeløb'',''Skadebeløb og værditab'') THEN COALESCE(n.areal_oversvoem_nutid_m2 * p.{f_price_t_agr_price} /100.00,0.0) ELSE 0.0 END + 
+              0.089925625   * CASE WHEN ''{Medtag i risikoberegninger}'' IN (''Skadebeløb'',''Skadebeløb og værditab'') THEN COALESCE(f.areal_oversvoem_fremtid_m2 * p.{f_price_t_agr_price} /100.00,0.0) ELSE 0.0 END
+          )/{Returperiode, antal år})::NUMERIC(12,2) AS {f_risk_q_agriculture},
+          '''' AS omraade
+    ) r
+	WHERE f.cnt_oversvoem_fremtid > 0 OR n.cnt_oversvoem_nutid > 0
+', 'P', '', '', '', '', 'SQL template for agriculture model ', 8, ' ')
+ON CONFLICT (name) DO UPDATE SET value = EXCLUDED.value;
+
